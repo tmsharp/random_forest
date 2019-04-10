debb40b8a6f26022d4d9eef672c6bafd5a76b7d1
tmsharp <tom.m.sharp12@gmail.com>
2019-04-10 18:57:34 -0400

    final notebook added

--- setup/environment_setup.bat
+++ setup/environment_setup.bat
@@ -1,4 +1,4 @@
 call %USERPROFILE%\AppData\Local\Continuum\anaconda3\Scripts\activate.bat
-conda create --name dm_ml --file requirements.txt -y
+conda create --name newenvenv --file requirements.txt -y
 
-python -m ipykernel install --user --name dm_ml --display-name "Demystifying ML"
+python -m ipykernel install --user --name newenvenv --display-name "newenvenvML"
